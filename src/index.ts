1f765f6480187ea5f0bc52b05eedff69441cc948
shardul <shardul.aeer@somaiya.edu>
2021-03-28 17:19:34 +0530

    feat: Read text fields and file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import express from 'express'
 import cors from 'cors'
+import multer from 'multer'
 
 const app = express()
 app.use(cors())
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+app.post('/upload', upload.array('zip'), (req, res) => {
+    console.log('text body', req.body)
+    const {micmacScript, email} = req.body
+    const files = req.files as Express.Multer.File[]
+    const zip = files.pop()
+    console.log('zip file',  zip)
+    res.send()
+});
 
 const port = Number(process.env.PORT || 3000)
 app.listen(port, () => {
