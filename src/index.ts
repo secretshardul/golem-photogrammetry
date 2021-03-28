35581f31a8e8bef9bb895e2f95df069ce15f89c1
shardul <shardul.aeer@somaiya.edu>
2021-03-28 17:28:55 +0530

    feat: Save uploaded zip file to disk

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,29 @@
 import express from 'express'
 import cors from 'cors'
 import multer from 'multer'
+// import fs from 'fs'
+import fs from 'fs/promises'
 
 const app = express()
 app.use(cors())
 
-const upload = multer({ storage: multer.memoryStorage() });
+const upload = multer({ storage: multer.memoryStorage() })
 
-app.post('/upload', upload.array('zip'), (req, res) => {
+app.post('/upload', upload.array('zip'), async (req, res) => {
     console.log('text body', req.body)
     const {micmacScript, email} = req.body
     const files = req.files as Express.Multer.File[]
     const zip = files.pop()
     console.log('zip file',  zip)
-    res.send()
+
+    if(zip && micmacScript && email) {
+        await fs.writeFile('download.zip', zip.buffer)
+        res.send()
+
+    } else {
+        res.sendStatus(400)
+    }
+
 });
 
 const port = Number(process.env.PORT || 3000)
