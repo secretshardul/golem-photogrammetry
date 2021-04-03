4e19ce73d47c015fcc106f744fc23107a0df05fe
shardul <shardul.aeer@somaiya.edu>
2021-04-03 22:33:36 +0530

    feat: Call API from form

--- src/index.ts
+++ src/index.ts
@@ -11,35 +11,31 @@ app.use(express.static(__dirname + '/public'))
 
 const upload = multer({ storage: multer.memoryStorage() })
 
-app.post('/index.html', upload.array('zip'), async (req, res) => {
-    res.redirect('/success.html')
-    // res.sendStatus(200)
+app.post('/', upload.array('zip'), async (req, res) => {
+    // res.redirect('/success.html')
 
-    // console.log('text body', req.body)
-    // const {micmacScript, email} = req.body
-    // const files = req.files as Express.Multer.File[]
-    // const zip = files.pop()
-    // console.log('zip file',  zip)
+    console.log('text body', req.body)
+    const {micmacScript, email} = req.body
+    const files = req.files as Express.Multer.File[]
+    const zip = files.pop()
+    console.log('zip file', zip)
 
-    // if(zip && micmacScript && email) {
-    //     // const fileName = 'download.zip'
-    //     const filePath = path.join(__dirname, zip.originalname)
-    //     const instructions = micmacScript.split('\n') as string[]
-    //     console.log('Instructions', instructions)
-    //     await fs.writeFile(filePath, zip.buffer)
-    //     res.send()
+    if(zip && micmacScript && email) {
+        const filePath = path.join(__dirname, zip.originalname)
+        const instructions = micmacScript.split('\n') as string[]
+        console.log('Instructions', instructions)
+        await fs.writeFile(filePath, zip.buffer)
+        res.redirect('/success.html')
 
-    //     try {
-    //         await generateMesh(zip.originalname, instructions)
-    //     } catch(error) {
-    //         console.log(error)
-    //         // TODO send error email
-    //     }
-
-
-    // } else {
-    //     res.sendStatus(400)
-    // }
+        try {
+            await generateMesh(zip.originalname, instructions)
+        } catch(error) {
+            console.log(error)
+            // TODO send error email
+        }
+    } else {
+        res.sendStatus(400)
+    }
 
 })
 
