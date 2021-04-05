b3feebb4e75905d23c2b06d46303c161b570b4e6
shardul <shardul.aeer@somaiya.edu>
2021-04-05 17:17:51 +0530

    style: Code editor for shell script

--- src/public/script.js
+++ src/public/script.js
@@ -4,3 +4,9 @@ async function sendData(event) {
   // var zipFile = document.getElementById("zip").value
   // console.log('Got file', zipFile)
 }
+
+const scriptEditor = document.getElementById('script-editor')
+const editor = CodeMirror.fromTextArea(scriptEditor, {
+  lineNumbers: true,
+  // theme: 'dracula',
+})
